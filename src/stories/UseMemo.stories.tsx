5f2829781ca2ddf91566f4b3fa4d4f031a56d347
korsakov <korsakov.ua.dn@gmail.com>
2021-07-26 22:23:43 +0300

    differences between useMemo and useCallback at Use.Memo.stories

--- src/stories/UseMemo.stories.tsx
+++ src/stories/UseMemo.stories.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, {useCallback, useState} from "react"
 import { useMemo } from "react";
 
 export default {
@@ -12,7 +12,6 @@ const UsersSecret = (props:{users: Array<string>}) => {
         <div>{props.users.map((u, i) => <div key={i}>{u}</div> )}</div>
     )
 }
-
 const Users = React.memo(UsersSecret)
 
 export const HelpsToReactMemo = () => {
@@ -38,3 +37,44 @@ export const HelpsToReactMemo = () => {
         </> 
     )
 }
+
+export const LikeUseCallback = () => {
+    console.log('LikeUseCallback');
+
+    let [count, setCounter] = useState(0)
+    let [books, setBooks] = useState(["React", "TS", "CSS"])
+
+    // const addBook = useMemo(() => {
+    //     return () => {
+    //         setBooks([...books, 'HTML' + new Date().getTime()])
+    //     }
+    // }, [books]) --- результат тот же
+    const addBook = useCallback(() => {
+            setBooks([...books, 'HTML' + new Date().getTime()])
+    }, [books])
+
+    return (
+        <>
+            <button onClick={() => setCounter(count + 1)} >+</button>
+
+            {count}
+            <Books addBook={addBook}/>
+        </>
+    )
+}
+const BooksSecret = (props:{addBook: ()=> void}) => {
+    console.log("BooksSecret");
+
+    return (
+        <div>
+            <button onClick={props.addBook} > add book </button>
+            {/*{props.books.map((book, i) => <div key={i}>{book}</div> )}*/}
+        </div>
+    )
+}
+const Books = React.memo(BooksSecret)
+
+// useEffect => logick, sideEffect => apiRequest, DOM, setTimeout
+// useCallback =>
+//     parentComponent => fn callback to dauther, useCallback
+//     daughterComponent
